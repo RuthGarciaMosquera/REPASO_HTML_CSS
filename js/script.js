ededf3c22df031a4051c6fd80086a2090bea7ade
RuthGarciaMosquera <ruth.garcia.mosquera@alumnos.udit.es>
2025-09-26 17:18:50 +0200

    let on click

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
 console.log("online");
 
-let test = $(".btns");
-console.log(test);
+// let test = $(".btns");
+// console.log(test);
+
+let $btnBuy = $("#btnBuy");
+$btnBuy.on("click", function(){
+    alert("Thanks for your purchase!");
+});
